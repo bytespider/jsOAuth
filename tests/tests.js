779b816ff7444e6e0cb8f46f3f99a828ee078a1f
Rob Griffiths <bytespider@live.co.uk>
2009-11-12 23:01:57 +0000

    added Url tests

--- tests/tests.js
+++ tests/tests.js
@@ -2,7 +2,33 @@ if (!fireunit) {
     throw new Error('FireUnit is required for unit testing.See http://fireunit.org/'); //http://fireunit.org/
 }
 
-fireunit.ok(jsOAuth('key', 'secret'), 'create consumer without new keyword');
-fireunit.ok(new jsOAuth('key', 'secret'), 'create consumer with new keyword');
+fireunit.ok(Url, 'Url is a class');
+
+// Only testing absolute Urls
+
+fireunit.ok(new Url('www.google.com'), 'www.google.com');
+fireunit.ok(new Url('www.google.com/'), 'www.google.com/');
+fireunit.ok(new Url('www.google.com/?'), 'www.google.com/?');
+fireunit.ok(new Url('www.google.com/search'), 'www.google.com/search');
+fireunit.ok(new Url('www.google.com/search?q=test'), 'http://www.google.com/search?q=test');
+fireunit.ok(new Url('www.google.com/search?q=test#anchor'), 'http://www.google.com/search?q=test#anchor');
+fireunit.ok(new Url('www.google.com/search?#anchor'), 'http://www.google.com/search?#anchor');
+fireunit.ok(new Url('www.google.com/search#anchor'), 'http://www.google.com/search#anchor');
+fireunit.ok(new Url('www.google.com/#anchor'), 'http://www.google.com/#anchor');
+fireunit.ok(new Url('www.google.com#anchor'), 'http://www.google.com#anchor');
+
+fireunit.ok(new Url('http://www.google.com'), 'http://www.google.com');
+fireunit.ok(new Url('http://www.google.com/'), 'http://www.google.com/');
+fireunit.ok(new Url('http://www.google.com/?'), 'http://www.google.com/?');
+fireunit.ok(new Url('http://www.google.com/search'), 'http://www.google.com/search');
+fireunit.ok(new Url('http://www.google.com/search?q=test'), 'http://www.google.com/search?q=test');
+fireunit.ok(new Url('http://www.google.com/search?q=test#anchor'), 'http://www.google.com/search?q=test#anchor');
+fireunit.ok(new Url('http://www.google.com/search?#anchor'), 'http://www.google.com/search?#anchor');
+fireunit.ok(new Url('http://www.google.com/search#anchor'), 'http://www.google.com/search#anchor');
+fireunit.ok(new Url('http://www.google.com/#anchor'), 'http://www.google.com/#anchor');
+fireunit.ok(new Url('http://www.google.com#anchor'), 'http://www.google.com#anchor');
+
+var url = new Url('www.google.com:443');
+fireunit.compare('https', url.scheme, 'Url: Correctly detected scheme is https from port (www.google.com:443)');
 
 fireunit.testDone();
