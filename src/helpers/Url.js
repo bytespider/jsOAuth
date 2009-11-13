b9743adc4e4f991015022840a41b868e7467138d
Rob Griffiths <bytespider@live.co.uk>
2009-11-13 23:10:22 +0000

    updated substitution tags

--- src/helpers/Url.js
+++ src/helpers/Url.js
@@ -1,11 +1,11 @@
 /**
+ * Url Helper
  * jsOAuth JavaScript OAuth Library v@VERSION
- * Url.js
  *
  * @preserve Copyright (c) 2009 Rob Griffiths
  *
- * Date: 
- * Revision: 
+ * Date: @DATE
+ * Revision: @REV
  */
 (function () {
     var 
@@ -18,8 +18,8 @@
          * @param {String} url
          */
         Url = function (url) {
-            var parsed_url, scheme, host, port, path, query, anchor, 
-            parser = /^([^:\/?#]+?:\/\/)*([^\/:?#]*)?(:[^\/?#]*)*([^?#]*)(\?[^#]*)?(#(.*))*/;
+            var parsed_url, scheme, host, port, path, query, anchor, params = {},
+                parser = /^([^:\/?#]+?:\/\/)*([^\/:?#]*)?(:[^\/?#]*)*([^?#]*)(\?[^#]*)?(#(.*))*/;
                 
             if (!(this instanceof arguments.callee)) {
                 return this.toString();
@@ -53,7 +53,7 @@
             this.host = host;
             this.port = port;
             this.path = path ? path : '/';
-            this.query = query ? query : '';
+			this.query = new QueryString();
             this.anchor = anchor ? anchor : '';
             
             /**
@@ -63,27 +63,72 @@
              * @return {String}
              */
             this.toString = function () {
+				var query = params
                 return scheme + '://' + host + path + (query ? '?' + query : '')  + (anchor ? '#' + anchor : '');
             };
-
-            
-            this.setQueryParams = function (query) {
-                var al = arguments.length, i;
-                if (al <= 1) {
+			
+			// finally call setQueryParams to set up params correctly
+			this.setQueryParams(query);
+			console.debug(params);
+        },
+		QueryString = function () {
+            if (!(this instanceof arguments.callee)) {
+                return this.toString();
+            }
+			
+			this.queryParams = {};
+			this.setQueryParams = function (query) {
+                var arg_length = arguments.length, i, query_array, 
+                    query_array_length, params = this.queryParams, key_value;
+                if (arg_length <= 1) {
                     if (typeof query === 'object') {
                         // iterate
+                        for (i in query) {
+                            if (query.hasOwnProperty(i)) {
+                                params[i] = query[i];
+                            }
+                        }
                     } else if (typeof query === 'string') {
                         // split string on '&'
+                        query_array = query.spilt('&');
                         // iterate over each of the array items
-                        // split on '=' to get key, value
+                        query_array_length = query_array.length;
+                        for (i = 0; i < query_array_length; i++) {
+                            // split on '=' to get key, value
+                            key_value = query_array[i].split('=');
+                            params[key_value[0]] = key_value[1];
+                        }
                     }
                 } else {
-                    for (i = 0; i < al; i = i + 2) {
+                    for (i = 0; i < arg_length; i += 2) {
                         // treat each arg as key, then value
+                        params[arguments[i]] = arguments[i+1];
                     }
                 }
+				
+				this.queryParams = params;
             };
-        };
+			
+            /**
+             * Returns the url query string
+             * 
+             * @memberOf QueryString
+             * @return {String}
+             */
+            this.toString = function () {
+                var query = [], i, params = this.queryParams;
+				for (i in params) {
+					if (params.hasOwnProperty(i)) {
+						query.push(i + '=' + params[i]);
+					}
+				}
+                return query.join('&');
+            };
+			
+			if (arguments.length > 0) {
+				this.setQueryParams(arguments);
+			}
+		};
     
     // Only give to the world if they want it
     if (window.Url === undefined) {
