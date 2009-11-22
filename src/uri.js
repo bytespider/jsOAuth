ee7446c7c9f7b08f9c89067d6f42ca185b8b97e3
Rob Griffiths <bytespider@live.co.uk>
2009-11-22 13:16:06 +0000

    add set bulk parameter add function setQueryParams

--- src/uri.js
+++ src/uri.js
@@ -170,6 +170,40 @@
         return ret;
     };
     
+    /**
+     * 
+     * @param {Object} query
+     */
+    QueryString.prototype.setQueryParams = function (query) {
+        var args = arguments, args_length = args.length, i, query_array, 
+            query_array_length, querystring = this, key_value;
+            
+        if (args_length == 1) {
+            if (typeof query === TYPEOF_OBJECT) {
+                // iterate
+                for (i in query) {
+                    if (query.hasOwnProperty(i)) {
+                        querystring[i] = query[i];
+                    }
+                }
+            } else if (typeof query === TYPEOF_STRING) {
+                // split string on '&'
+                query_array = query.split('&');
+                // iterate over each of the array items
+                for (i = 0, query_array_length = query_array.length; i < query_array_length; i++) {
+                    // split on '=' to get key, value
+                    key_value = query_array[i].split('=');
+                    querystring[key_value[0]] = key_value[1];
+                }
+            }
+        } else {
+            for (i = 0; i < arg_length; i += 2) {
+                // treat each arg as key, then value
+                querystring[args[i]] = args[i+1];
+            }
+        }
+    };
+    
     /** 
      * rfc3986 compatable encode of a string
      * 
