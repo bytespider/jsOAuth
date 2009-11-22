5d5c9f2b14a37a128f8d401d3782108e81c8cd80
Rob Griffiths <bytespider@live.co.uk>
2009-11-22 17:20:06 +0000

    clean up Query String

--- src/uri.js
+++ src/uri.js
@@ -66,73 +66,11 @@
         };
     };
     
-    QueryString = function () {
-        var args = arguments, args_callee = args.callee, args_length = args.length, 
-            QueryString = this;
-        
-        if (!(this instanceof args_callee)) {
-            return this.toString();
-        }
-        
-        QueryString.queryParams = {};
-        /**
-         * 
-         * @param {Object} query
-         */
-        QueryString.setQueryParams = function (query) {
-            var args = arguments, args_length = args.length, i, query_array, 
-                query_array_length, params = QueryString.queryParams, key_value;
-            if (args_length == 1) {
-                if (typeof query === TYPEOF_OBJECT) {
-                    // iterate
-                    for (i in query) {
-                        if (query.hasOwnProperty(i)) {
-                            params[i] = query[i];
-                        }
-                    }
-                } else if (typeof query === TYPEOF_STRING) {
-                    // split string on '&'
-                    query_array = query.split('&');
-                    // iterate over each of the array items
-                    for (i = 0, query_array_length = query_array.length; i < query_array_length; i++) {
-                        // split on '=' to get key, value
-                        key_value = query_array[i].split('=');
-                        params[key_value[0]] = key_value[1];
-                    }
-                }
-            } else {
-                for (i = 0; i < arg_length; i += 2) {
-                    // treat each arg as key, then value
-                    params[args[i]] = args[i+1];
-                }
-            }
-            
-            QueryString.queryParams = params;
-        };
-        
-        /**
-         * Returns the url query string
-         * 
-         * @memberOf QueryString
-         * @return {String}
-         */
-        QueryString.toString = function () {
-            var query = [], i, params = QueryString.queryParams;
-            for (i in params) {
-                if (params.hasOwnProperty(i) && params[i] !== UNDEFINED) {
-                    query.push(i + '=' + params[i]);
-                }
-            }
-            return query.join('&');
-        };
-        
-        if (args_length > 0) {
-            QueryString.setQueryParams(args);
-        }
-        
-        return QueryString;
-    };
-   
+    /**
+     * Create and manage a query string
+     * 
+     * @param {Object} obj
+     */
     QueryString = function(obj){
         var args = arguments, args_callee = args.callee, args_length = args.length,
             i, querystring = this;
