32c60459861e5c46fad6094e99c070315a243b82
Rob Griffiths <bytespider@live.co.uk>
2010-01-26 22:53:04 +0000

    attempt to write sha1 hash function

--- src/OAuth/SignatureMethod.js
+++ src/OAuth/SignatureMethod.js
@@ -9,12 +9,23 @@ function OAuthSignatureMethod() {
     this.hash = function(message){};
     
     this.hmac = function(key, message) {
+        //key = toByteString(key);
+        //message = toByteString(message);
         if (key.length > this.blocksize) {
             key = this.hash(key);
         } else if (key.length < this.blocksize) {
-            key = key + (new Array(this.blocksize - key.length)).join(0);
+            key = key + ((this.blocksize - key.length) * 0x00);
         }
+        
+        console.log(key);
+        
         return this.hash(key ^ (0x5c * this.blocksize) + 
             this.hash((0x36 * this.blocksize) + message));
     };
+    
+    function toByteString(str) {
+        return str.replace(/./g, function(s){
+            return s.charCodeAt(0) & 0xFF;
+        });
+    }
 }
--- src/OAuth/SignatureMethod/HMAC-SHA1.js
+++ src/OAuth/SignatureMethod/HMAC-SHA1.js
@@ -1,13 +1,24 @@
 function OAuthSignatureMethodHMACSHA1() {
     this.name = 'HMAC-SHA1';
     this.sign = function(consumer_secret, token_secret){
+        this.hmac(consumer_secret, '');
         var signature = [];
         return signature.join('&');
     };
     
     this.blocksize = 64;
-    this.hash(message) {
-        
+    this.hash = function (message) {
+        var h0 = 0x67452301;
+        var h1 = 0xEFCDAB89;
+        var h2 = 0x98BADCFE;
+        var h3 = 0x10325476;
+        var h4 = 0xC3D2E1F0;
+
+        message = message.split('');
+        var bytes = message.length * 8;
+        message[bytes >> 5] |= 0x80 << 24 - bytes % 32;
+        message[(bytes + 64 >>> 9 << 4) + 15] = bytes;
+        alert(message);
     }
 }
 
