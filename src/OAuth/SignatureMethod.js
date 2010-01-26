48ec9986d00ab849e58d72939436c78e1d7cc56f
Rob Griffiths <bytespider@live.co.uk>
2010-01-26 21:35:41 +0000

    moved hmac to OAuth/SignatureMethod

--- src/OAuth/SignatureMethod.js
+++ src/OAuth/SignatureMethod.js
@@ -4,4 +4,17 @@ function OAuthSignatureMethod() {
         return this.name;
     };
     this.sign = function(){};
+    
+    this.blocksize = 64;
+    this.hash = function(message){};
+    
+    this.hmac = function(key, message) {
+        if (key.length > this.blocksize) {
+            key = this.hash(key);
+        } else if (key.length < this.blocksize) {
+            key = key + (new Array(this.blocksize - key.length)).join(0);
+        }
+        return this.hash(key ^ (0x5c * this.blocksize) + 
+            this.hash((0x36 * this.blocksize) + message));
+    };
 }
