18c77d15c530662f7a97a174a146e2bb0b0fcff4
Rob Griffiths <bytespider@live.co.uk>
2010-02-17 13:13:45 +0000

    added cache object so that toSting returns json - may push this down to a prototype

---
+++ src/OAuth/Cache.js
@@ -0,0 +1,11 @@
+function OAuthCache(options) {
+    if (arguments.length > 0) {
+        this.init(options);
+    }
+    
+    this.init = function (options) {/*stub*/};
+    
+    this.toString = function () {
+        return this.toSource();
+    }
+}
