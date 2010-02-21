047872fe6e302ec0fd03c92bb3d04447f26c917f
unknown <Rob@.(none)>
2010-02-21 16:19:23 +0000

    added urlDecode

--- src/OAuth/Utilities.js
+++ src/OAuth/Utilities.js
@@ -15,8 +15,17 @@ OAuthUtilities.urlEncode = function(string){
     return string_arr.join('');
 };
 
+OAuthUtilities.urlDecode = function(string){
+	if (!string) return string;
+                          
+    return string.replace(/%[a-fA-F0-9]{2}/ig, function (match) {
+		return String.fromCharCode(parseInt(match.replace('%', ''), 16));
+	});
+};
+
 OAuthUtilities.toByteString = function(str) {
     return str.replace(/./g, function(s){
         return s.charCodeAt(0).toString(16);
     });
 }
+
