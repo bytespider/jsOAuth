b998cade79f24219883d8fcf3cf78b16ed5e975a
Rob Griffiths <bytespider@live.co.uk>
2010-02-23 16:16:21 +0000

    updated cache to save and load from local storage

--- src/OAuth/Cache.js
+++ src/OAuth/Cache.js
@@ -3,9 +3,24 @@ function OAuthCache(options) {
         this.init(options);
     }
     
+    var cache_id = '';
+    var cache_data;
+
     this.init = function (options) {/*stub*/};
     
+    this.load = function (id) {
+        cache_id = id || cache_id || '';
+        cache_data = window.localStorage.getItem(cache_id);
+        return cache_data;
+    };
+
+    this.save = function (data, id) {
+        cache_id = id || cache_id || '';
+        cache_data = data ? cache_data : {};
+        window.localStorage.setItem(cache_id, cache_data);
+    };
+    
     this.toString = function () {
-        return this.toSource();
+        return cache_data;
     }
 }
