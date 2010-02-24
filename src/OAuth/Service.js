812a7ae7289d191b75f217b564e477c980ccb286
Rob Griffiths <bytespider@live.co.uk>
2010-02-24 15:30:42 +0000

    updating API for version 0.5 to be simpler to use

--- src/OAuth/Service.js
+++ src/OAuth/Service.js
@@ -1,16 +1,22 @@
 function OAuthService(options) {
     var parent = OAuthService.prototype;
     
-    if (arguments.length > 0) {
-        this.init(options);
-    }
+    this.signature_method               = 'PLAINTEXT';
+	
+	this.realm                          = '';
+    this.requestTokenUrl                = '';
+    this.authorizationUrl               = '';
+    this.accessTokenUrl                 = '';
+    this.authenticationUrl              = '';
     
     this.init = function(options) {
         this.onauthorized = options.onauthorized;
         parent.init.apply(this, arguments);
     };
                
-    this.signature_method = 'PLAINTEXT'; 
+    if (arguments.length > 0) {
+        this.init(options);
+    }
 }
 
 OAuthService.prototype = new OAuthConsumer();
