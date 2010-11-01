79502fc258c93017ba15b608d8481cb12e87dc2f
Rob Griffiths <rob@bytespider.eu>
2010-11-01 09:10:12 +0000

    start file

--- src/start.js
+++ src/start.js
@@ -1,5 +1,8 @@
-(function (window) {
-    var document = window.document;
+/**
+ *  @license
+ *  jsOAuth version @VERSION
+ *  Copyright (c) 2010 Rob Griffiths (http://bytespider.eu)
+ *  jsOAuth is freely distributable under the terms of an MIT-style license.
+ */
+(function (global) {
     
-    var JS = {};
-    window['JS'] = JS;
