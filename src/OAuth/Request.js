10ddc77ac41e1d38b7202bef7db020f0d95079a8
Rob Griffiths <rob@bytespider.eu>
2010-11-11 17:01:50 +0000

    unit test: check call works

--- src/OAuth/Request.js
+++ src/OAuth/Request.js
@@ -2,15 +2,10 @@
 	 * Factory object for XMLHttpRequest
 	 */
 	function Request() {
-		if (XMLHttpRequest) {
-			return new XMLHttpRequest();
-		}
-
 		// CommonJS require
-		if (require) {
-			return require("xhr").XMLHttpRequest;
+		if (undefined != require) {
+			var XMLHttpRequest = require("xhr").XMLHttpRequest;
 		}
 
-		// nothing compatable with the W3C standard
-		return false;
+		return new XMLHttpRequest();
 	}
