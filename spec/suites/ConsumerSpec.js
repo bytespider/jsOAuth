ae79aa0147710356b9ea4fc52b562aba25d8613d
Rob Griffiths <rob@bytespider.eu>
2010-12-03 09:21:13 +0000

    Updated test suite

--- spec/suites/ConsumerSpec.js
+++ spec/suites/ConsumerSpec.js
@@ -7,3 +7,21 @@ test('Output URL Encode', function () {
     equals(OAuth.urlEncode(''), '', 'Output test 1');
     equals(OAuth.urlEncode('$ & < > ? ; # : = , " \' ~ + %'), '%24%20%26%20%3C%20%3E%20%3F%20%3B%20%23%20%3A%20%3D%20%2C%20%22%20%27%20~%20%2B%20%25', 'Output test 2');
 });
+
+test('OAuth 2-Legged Request', function () {
+    equals(OAuth.urlEncode(''), '', 'Output test 1');
+    equals(OAuth.urlEncode('$ & < > ? ; # : = , " \' ~ + %'), '%24%20%26%20%3C%20%3E%20%3F%20%3B%20%23%20%3A%20%3D%20%2C%20%22%20%27%20~%20%2B%20%25', 'Output test 2');
+
+    asyncTest("request", function() {
+        stop(5000);
+        var oauth = OAuth({
+            debug: true,
+            consumerKey: 'ba9df9055c77f338',
+            consumerSecret: '846ffe1ec3b18989e73fe7fff833'
+        });
+        oauth.get('http://oauth-sandbox.sevengoslings.net/two_legged', function (data) {
+            equals(data.text, 'SUCCESS! This is a 2-legged call from the `jsOAuth2` consumer which was made by `bytespider`.', 'Request success');
+            start();
+        });
+    });
+});
