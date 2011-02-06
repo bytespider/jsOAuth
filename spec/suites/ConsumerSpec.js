80fc9d885caa84d6aa5ffd9401a18127f74ca288
Rob Griffiths <rob@bytespider.eu>
2011-02-06 19:43:59 +0000

    added umlaut tests

--- spec/suites/ConsumerSpec.js
+++ spec/suites/ConsumerSpec.js
@@ -8,6 +8,7 @@ test('Output URL Encode', function () {
     equals(OAuth.urlEncode('$ & < > ? ; # : = , " \' ~ + %'), '%24%20%26%20%3C%20%3E%20%3F%20%3B%20%23%20%3A%20%3D%20%2C%20%22%20%27%20~%20%2B%20%25', 'Output test 2');
     
     equals(OAuth.urlEncode('ß'), '%C3%9F', 'Output test 3');
+    equals(OAuth.urlEncode('ü'), '%C3%BC', 'Output test 4');
 });
 
 asyncTest("OAuth 2-Legged Request", function() {
