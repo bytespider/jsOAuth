d0b5551805e10985c72cab2c780098b0f040dfba
Rob Griffiths <rob@bytespider.eu>
2011-02-08 20:37:21 +0000

    use prototype to make code quicker

--- src/OAuth/Collection.js
+++ src/OAuth/Collection.js
@@ -15,59 +15,65 @@
 	    return collection;
 	};
 	 
-	function Hash() {
-	    var hash = this;
-	     
-	    hash.join = function(string){
-	        string = string || '';
-	        return this.values().join(string);
-	    };
-	    //hash.reverse = function(){};
-	    hash.keys = function(){
-	        var i, arr = [], self = this;
-	        for (i in self) {
-	            if (self.hasOwnProperty(i)) {
-	                arr.push(i);
-	            }
-	        }
-	         
-	        return arr;
-	    };
-	    hash.values = function(){
-	        var i, arr = [], self = this;
-	        for (i in self) {
-	            if (self.hasOwnProperty(i)) {
-	                arr.push(self[i]);
-	            }
-	        }
-	         
-	        return arr; 
-	    };
-	    hash.shift = function(){throw 'not implimented'};
-	    hash.unshift = function(){throw 'not implimented'};
-	    hash.push = function(){throw 'not implimented'};
-	    hash.pop = function(){throw 'not implimented'};
-	    hash.sort = function(){throw 'not implimented'};
-	     
-	    hash.ksort = function(func){
-	        var self = this, keys = self.keys(), i, value, key;
-	         
-	        if (func == undefined) {
-	            keys.sort();
-	        } else {
-	            keys.sort(func);
-	        }
-	         
-	        for (i = 0; i  < keys.length; i++) {
-	            key = keys[i];
-	            value = self[key];
-	            delete self[key];
-	            self[key] = value;
-	        }
-	         
-	        return self;
-	    };
-	     
-	    return hash;
-	}
+	function Hash() {}
+	Hash.prototype = {
+		join: function(string){
+		    string = string || '';
+		    return this.values().join(string);
+		},
+		keys: function(){
+		    var i, arr = [], self = this;
+		    for (i in self) {
+		        if (self.hasOwnProperty(i)) {
+		            arr.push(i);
+		        }
+		    }
+		     
+		    return arr;
+		},
+		values: function(){
+		    var i, arr = [], self = this;
+		    for (i in self) {
+		        if (self.hasOwnProperty(i)) {
+		            arr.push(self[i]);
+		        }
+		    }
+		     
+		    return arr; 
+		},
+		shift: function(){throw 'not implimented'},
+		unshift: function(){throw 'not implimented'},
+		push: function(){throw 'not implimented'},
+		pop: function(){throw 'not implimented'},
+		sort: function(){throw 'not implimented'},
+		 
+		ksort: function(func){
+		    var self = this, keys = self.keys(), i, value, key;
+		     
+		    if (func == undefined) {
+		        keys.sort();
+		    } else {
+		        keys.sort(func);
+		    }
+		     
+		    for (i = 0; i  < keys.length; i++) {
+		        key = keys[i];
+		        value = self[key];
+		        delete self[key];
+		        self[key] = value;
+		    }
+		     
+		    return self;
+		},
+		toObject: function () {
+			var obj = {}, i, self = this;;
+			for (i in self) {
+				if (self.hasOwnProperty(i)) {
+				    obj[i] = self[i];
+				}
+			}
+			
+			return obj;
+		}
+	};
 	Collection.prototype = new Hash;
