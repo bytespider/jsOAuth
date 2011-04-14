0dfd9d2b4e4efb0132788899abf978a964f0e95e
Sam Wilson <tecywiz121@hotmail.com>
2011-04-14 14:52:53 -0400

    Fixed some JSLint issues

--- src/OAuth/Crypto.js
+++ src/OAuth/Crypto.js
@@ -1,3 +1,57 @@
+    function zeroPad(length) {
+        var arr = new Array(++length);
+        return arr.join(0).split('');
+    }
+
+    function stringToByteArray(str) {
+        var bytes = [], code, i;
+        
+        for(i = 0; i < str.length; i++) {
+            code = str.charCodeAt(i);
+            
+            if (code < 128) {
+                bytes.push(code);
+            } else if (code < 2048) {
+                bytes.push(192+(code>>6), 128+(code&63));
+            } else if (code < 65536) {
+                bytes.push(224+(code>>12), 128+((code>>6)&63), 128+(code&63));
+            } else if (code < 2097152) {
+                bytes.push(240+(code>>18), 128+((code>>12)&63), 128+((code>>6)&63), 128+(code&63));
+            }
+        }
+        
+        return bytes;
+    }
+
+    function wordsToByteArray(words) {
+        var bytes = [], i;
+        for (i = 0; i < words.length * 32; i += 8) {
+            bytes.push((words[i >>> 5] >>> (24 - i % 32)) & 255);
+        }
+        return bytes;
+    }
+
+    function byteArrayToHex(byteArray) {
+        var hex = [], l = byteArray.length, i;
+        for (i = 0; i < l; i++) {
+            hex.push((byteArray[i] >>> 4).toString(16));
+            hex.push((byteArray[i] & 0xF).toString(16));
+        }
+        return hex.join('');
+    }
+
+    function byteArrayToString(byteArray) {
+        var string = '', l = byteArray.length, i;
+        for (i = 0; i < l; i++) {
+            string += String.fromCharCode(byteArray[i]);
+        }
+        return string;
+    }
+
+    function leftrotate(value, shift) {
+        return (value << shift) | (value >>> (32 - shift));
+    }
+
     function SHA1(message) {
         if (message !== undefined) {
             var m = message, crypto, digest;
@@ -137,58 +191,4 @@
             return byteArrayToHex(byteArray);
         }
         return byteArrayToString(byteArray);
-    }
-
-    function zeroPad(length) {
-        var arr = new Array(++length);
-        return arr.join(0).split('');
-    }
-
-    function stringToByteArray(str) {
-        var bytes = [], code;
-        
-        for(i = 0; i < str.length; i++) {
-            code = str.charCodeAt(i);
-            
-            if (code < 128) {
-            	bytes.push(code);
-            } else if (code < 2048) {
-            	bytes.push(192+(code>>6), 128+(code&63));
-            } else if (code < 65536) {
-            	bytes.push(224+(code>>12), 128+((code>>6)&63), 128+(code&63));
-            } else if (code < 2097152) {
-            	bytes.push(240+(code>>18), 128+((code>>12)&63), 128+((code>>6)&63), 128+(code&63));
-            }
-        }
-        
-        return bytes;
-    }
-
-    function wordsToByteArray(words) {
-        var bytes = [], i;
-        for (i = 0; i < words.length * 32; i += 8) {
-            bytes.push((words[i >>> 5] >>> (24 - i % 32)) & 255);
-        }
-        return bytes;
-    }
-
-    function byteArrayToHex(byteArray) {
-        var hex = [], l = byteArray.length, i;
-        for (i = 0; i < l; i++) {
-            hex.push((byteArray[i] >>> 4).toString(16));
-            hex.push((byteArray[i] & 0xF).toString(16));
-        }
-        return hex.join('');
-    }
-
-    function byteArrayToString(byteArray) {
-        var string = '', l = byteArray.length, i;
-        for (i = 0; i < l; i++) {
-            string += String.fromCharCode(byteArray[i]);
-        }
-        return string;
-    }
-
-    function leftrotate(value, shift) {
-        return (value << shift) | (value >>> (32 - shift));
     }
