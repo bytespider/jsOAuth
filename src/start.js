6702a66f599f26e98143defcab6d42e65a71c5cb
Rob Griffiths <rob@bytespider.eu>
2011-05-04 06:46:44 +0100

    updated copyright

--- src/start.js
+++ src/start.js
@@ -1,7 +1,7 @@
 /**
  *  @license
  *  jsOAuth version @VERSION
- *  Copyright (c) 2010 Rob Griffiths (http://bytespider.eu)
+ *  Copyright (c) 2010, 2011 Rob Griffiths (http://bytespider.eu)
  *  jsOAuth is freely distributable under the terms of an MIT-style license.
  */
 var exports = exports || this;
