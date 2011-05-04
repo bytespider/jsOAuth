649a0dbc62e5202a3fac06596ff4e51cb40a867b
Rob Griffiths <rob@bytespider.eu>
2011-05-04 06:47:09 +0100

    fixed test for updated urlEncode

--- spec/suites/ConsumerSpec.js
+++ spec/suites/ConsumerSpec.js
@@ -5,7 +5,7 @@ test('Basics', function () {
 
 test('Output URL Encode', function () {
     equals(OAuth.urlEncode(''), '', 'Output test 1');
-    equals(OAuth.urlEncode("\r\n $ & < > ? ; # : = , " \' ~ + %"), '%0D%0A%20 %24%20%26%20%3C%20%3E%20%3F%20%3B%20%23%20%3A%20%3D%20%2C%20%22%20%27%20~%20%2B%20%25', 'Output test 2');
+    equals(OAuth.urlEncode("\r\n $ & < > ? ; # : = , \" ' ~ + %"), '%0D%0A%20%24%20%26%20%3C%20%3E%20%3F%20%3B%20%23%20%3A%20%3D%20%2C%20%22%20%27%20~%20%2B%20%25', 'Output test 2');
     
     equals(OAuth.urlEncode('ß'), '%C3%9F', 'Output test 3');
     equals(OAuth.urlEncode('ü'), '%C3%BC', 'Output test 4');
