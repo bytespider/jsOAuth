034fa1d43011f377fa29eb3636cc4a396f2889dc
Rob Griffiths <rob@bytespider.eu>
2011-10-23 15:41:56 +0100

    reflect XMLHttpRequest

--- src/OAuth/Request/Request.js
+++ src/OAuth/Request/Request.js
@@ -1,33 +1,93 @@
 
-function OAuthRequest(transport) {
+function OAuthRequest() {
+    this.request = OAuthRequest.factory();
 }
+
+OAuthRequest.factory = function () {
+    for (var transport in OAuthRequest.Transports)
+    {
+        if (OAuthRequest.Transports[transport].test === true)
+        {
+            return OAuthRequest.Transports[transport].factory();
+        }
+    }
+};
 
 OAuthRequest.prototype = {
     constructor: OAuthRequest,
-    onreadystatechange: "",
-    onerror: "",
-    readyState: OAuthRequest.UNSENT,
+    set onreadystatechange(handler) {
+        this.request.onreadystatechange = handler;
+    },
+    get onreadystatechange() {
+        return this.request.onreadystatechange;
+    },
+    set onerror(handler) {
+        this.request.onerror = handler;
+    },
+    get onerror() {
+        return this.request.onerror;
+    },
+    get readyState() {
+        return this.request.readyState;
+    },
 
     // request
-    open: function (method, url, async, user, password) {},
-    setRequestHeader: function (header, value) {},
-    send: function (data) {},
-    abort: function () {},
+    open: function (method, url, async, user, password) {
+        var xhr = this.request;
+
+        xhr.method = method;
+        xhr.url = url;
+        xhr.async = async;
+        xhr.user = password;
+
+        xhr.open(xhr.method, xhr.url, xhr.async, xhr.user, xhr.password);
+    },
+    setRequestHeader: function (header, value) {
+        this.request.headers[header] = value;
+        this.request.setRequestHeader(header, value);
+    },
+    send: function (data) {
+        var xhr = this.request;
+
+        xhr.send(data);
+    },
+    abort: function () {
+        this.request.abort();
+    },
 
     // response
-    status: 0,
-    statusText: "",
-    getResponseHeader: function (header) {},
-    getAllResponseHeaders: function () {},
-    responseText: "",
-    responseXML: "",
-    addEventListener: function (type, handler, useCapture) {}.
-    removeEventListener: function (type, handler, useCapture) {},
-    dispatchEvent: function (event) {}
-};
+    get status() {
+        return this.request.status;
+    },
+    get statusText() {
+        return this.request.statusText;
+    },
+    getResponseHeader: function (header) {
+        this.request.getResponseHeader(header);
+    },
+    getAllResponseHeaders: function () {
+        this.request.getAllResponseHeaders();
+    },
+    get responseText () {
+        return this.request.responseText;
+    },
+    get responseXML () {
+        return this.request.responseXML;
+    },
 
-OAuthRequest.UNSENT = 0;
-OAuthRequest.OPENED = 1;
-OAuthRequest.HEADERS_RECEIVED = 2;
-OAuthRequest.LOADING = 3;
-OAuthRequest.DONE = 4;
+    UNSENT: 0,
+    OPENED: 1,
+    HEADERS_RECEIVED: 2,
+    LOADING: 3,
+    DONE: 4,
+
+    addEventListener: function (type, handler, useCapture) {
+        this.request.addEventListener(type, handler, useCapture);
+    },
+    removeEventListener: function (type, handler, useCapture) {
+        this.request.removeEventListener(type, handler, useCapture);
+    },
+    dispatchEvent: function (event) {
+        this.request.dispatchEvent(event);
+    }
+};
