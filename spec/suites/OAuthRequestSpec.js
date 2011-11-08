53ad1d94fe5caf4698a55ce498f2ca9bba23e063
Rob Griffiths <rob@bytespider.eu>
2011-11-08 21:44:34 +0000

    Modify to be jasmine compatable

--- spec/suites/OAuthRequestSpec.js
+++ spec/suites/OAuthRequestSpec.js
@@ -1,51 +1,76 @@
-module('OAuth Consumer');
-test('XHR compatability', function () {
-    ok(OAuthRequest !== undefined, 'Global object exists');
-
-    var xhr = new OAuthRequest();
-    ok(xhr instanceof OAuthRequest);
+beforeEach(function() {
+    this.addMatchers({
+        toBeInstanceOf: function(type) {
+            return this.actual instanceof type;
+        }
+    })
 });
 
-asyncTest("Request a page", function() {
-    var xhr = new OAuthRequest();
-    xhr.open('GET', 'http://www.google.co.uk', true);
+describe('OAuth XMLHttpRequest', function () {
+    it('Should exist', function () {
+        var OAuthRequest = window.OAuthRequest;
 
-    xhr.addEventListener('readystatechange', function (event) {
-        if (this.readyState == this.DONE && this.status >= 200)
-        {
-            start();
-        }
+        expect(OAuthRequest).toBeDefined();
+
+        var xhr = new OAuthRequest;
+        expect(xhr).toBeInstanceOf(OAuthRequest);
     });
 
-    xhr.send(null);
+    it("Should request a page", function() {
+        var xhr = new OAuthRequest(),
+            done = false;
+
+        xhr.open('GET', 'http://www.google.co.uk', true);
+
+        xhr.addEventListener('readystatechange', function (event) {
+            if (this.readyState == this.DONE && this.status >= 200)
+            {
+                done = true;
+            }
+        });
+
+        waitsFor(function () {
+            return done;
+        }, "Requet never completed", 10000);
+
+        xhr.send(null);
+    });
+
+    it("Should request a page using jQuery.ajax", function() {
+        var xhr = new OAuthRequest(),
+            done = false;
+
+        waitsFor(function () {
+            return done;
+        }, "Requet never completed", 10000);
+
+        jQuery.ajaxSettings.xhr =  function () { return new OAuthRequest };
+        $.ajax("http://www.google.co.uk", {success: function (data, textStatus, jqXHR) {
+            done = true;
+        }});
+    });
+
+    it("Should request the authenticated users's timeine", function() {
+        var xhr = new OAuthRequest(),
+            done = true;
+
+        jQuery.ajaxSettings.xhr =  function () {
+            var xhr =  new OAuthRequest;
+            xhr.consumerKey = consumerKey;
+            xhr.consumerSecret = consumerSecret;
+            xhr.accessTokenKey = accessTokenKey;
+            xhr.accessTokenSecret = accessTokenSecret;
+
+            return xhr;
+        };
+
+        waitsFor(function () {
+            return done;
+        }, "Requet never completed", 10000);
+
+        $.ajax("http://api.twitter.com/1/statuses/home_timeline.json", {success: function (data, textStatus, jqXHR) {
+            done = true;
+        }});
+    });
+
 });
-
-asyncTest("Request a page using jQuery.ajax", function() {
-    var xhr = new OAuthRequest();
-    ok(xhr instanceof OAuthRequest);
-
-    jQuery.ajaxSettings.xhr =  function () { return new OAuthRequest };
-    $.ajax("http://www.google.co.uk", {success: function (data, textStatus, jqXHR) {
-        start();
-    }});
-});
-
-asyncTest("Request the authenticated users's timeine", function() {
-    var xhr = new OAuthRequest();
-    ok(xhr instanceof OAuthRequest);
-
-    jQuery.ajaxSettings.xhr =  function () {
-        var xhr =  new OAuthRequest;
-        xhr.consumerKey = consumerKey;
-        xhr.consumerSecret = consumerSecret;
-        xhr.accessTokenKey = accessTokenKey;
-        xhr.accessTokenSecret = accessTokenSecret;
-
-        return xhr;
-    };
-    $.ajax("http://api.twitter.com/1/statuses/home_timeline.json", {success: function (data, textStatus, jqXHR) {
-        console.log(data);
-        start();
-    }});
-});
-
--- spec/suites/UrlSpec.js
+++ spec/suites/UrlSpec.js
@@ -1,42 +1,40 @@
-module('Url');
+describe('Url parsing', function () {
+    var parse_tests = {
+        'HTTP://www.example.com/' : 'http://www.example.com/',
+        'http://www.ExAmPlE.com/' : 'http://www.example.com/',
+        'http://user:pw@www.ExAmPlE.com/' : 'http://user:pw@www.example.com/',
+        'http://USER:PW@www.ExAmPlE.com/' : 'http://USER:PW@www.example.com/',
+        'HTTP://X.COM/Y' : 'http://x.com/Y',
 
-var parse_tests = {
-    'HTTP://www.example.com/' : 'http://www.example.com/',
-    'http://www.ExAmPlE.com/' : 'http://www.example.com/',
-    'http://user:pw@www.ExAmPlE.com/' : 'http://user:pw@www.example.com/',
-    'http://USER:PW@www.ExAmPlE.com/' : 'http://USER:PW@www.example.com/',
-    'HTTP://X.COM/Y' : 'http://x.com/Y',
+        'http://x...y...#p': 'http://x...y...#p',
+        'http://x/p/"quoted"': 'http://x/p/',
+        'http://www.narwhaljs.org/blog/categories?id=news' : 'http://www.narwhaljs.org/blog/categories?id=news',
 
-    'http://x...y...#p': 'http://x...y...#p',
-    'http://x/p/"quoted"': 'http://x/p/',
-    'http://www.narwhaljs.org/blog/categories?id=news' : 'http://www.narwhaljs.org/blog/categories?id=news',
+        'file:///etc/passwd' : 'file:///etc/passwd',
+        'file://localhost/etc/passwd' : 'file://localhost/etc/passwd',
+        'file://foo/etc/passwd' : 'file://foo/etc/passwd',
+        'file:///etc/node/' : 'file:///etc/node/',
+        'file://localhost/etc/node/' : 'file://localhost/etc/node/',
+        'file://foo/etc/node/' : 'file://foo/etc/node/',
+        'http://user:pass@example.com:8000/foo/bar?baz=quux#frag' : 'http://user:pass@example.com:8000/foo/bar?baz=quux#frag',
 
-    'file:///etc/passwd' : 'file:///etc/passwd',
-    'file://localhost/etc/passwd' : 'file://localhost/etc/passwd',
-    'file://foo/etc/passwd' : 'file://foo/etc/passwd',
-    'file:///etc/node/' : 'file:///etc/node/',
-    'file://localhost/etc/node/' : 'file://localhost/etc/node/',
-    'file://foo/etc/node/' : 'file://foo/etc/node/',
-    'http://user:pass@example.com:8000/foo/bar?baz=quux#frag' : 'http://user:pass@example.com:8000/foo/bar?baz=quux#frag',
+        'http://bucket_name.s3.amazonaws.com/image.jpg': 'http://bucket_name.s3.amazonaws.com/image.jpg',
+        'git+http://github.com/joyent/node.git': 'git+http://github.com/joyent/node.git',
 
-    'http://bucket_name.s3.amazonaws.com/image.jpg': 'http://bucket_name.s3.amazonaws.com/image.jpg',
-    'git+http://github.com/joyent/node.git': 'git+http://github.com/joyent/node.git',
+        //While this may seem counter-intuitive, a browser will parse
+        //<a href='www.google.com'> as a path.
+        'www.example.com' : 'http://www.example.com',
+    };
 
-    //While this may seem counter-intuitive, a browser will parse
-    //<a href='www.google.com'> as a path.
-    'www.example.com' : 'http://www.example.com',
-};
+    for (var u in parse_tests) {
+        it(u, function () {
+            var Url = window.Url;
+            var test = u;
 
+            var actual = Url.parse(test).toString(),
+            expected = parse_tests[test];
 
-for (var u in parse_tests) {
-
-    test(u, (function () {
-        var url = u;
-        return function() {
-            var actual = Url.parse(url).toString(),
-            expected = parse_tests[url];
-
-            deepEqual(actual, expected);
-        }
-    })());
-}
+            expect(expected).toEqual(actual);
+        });
+    }
+});
