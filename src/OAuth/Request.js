c8e8c7eaad317f378e7987d467b7b27c7d82d50a
Rob Griffiths <rob@bytespider.eu>
2012-04-02 17:56:42 +0200

    reworked to make sure the module is installed;

--- src/OAuth/Request.js
+++ src/OAuth/Request.js
@@ -9,10 +9,23 @@
             XHR = global.Titanium.Network.createHTTPClient();
         } else if (typeof require !== 'undefined') {
             // CommonJS require
-            XHR = new require("xhr").XMLHttpRequest();
-        } else {
+            try {
+                XHR = new require("xhr").XMLHttpRequest();
+            } catch (e) {
+                // module didn't expose correct API or doesn't exists
+                if (typeof global.XMLHttpRequest !== "undefined") {
+                    XHR = new global.XMLHttpRequest();
+                } else {
+                    throw "No valid request transport found.";
+                    return null;
+                }
+            }
+        } else if (typeof global.XMLHttpRequest !== "undefined") {
             // W3C
             XHR = new global.XMLHttpRequest();
+        } else {
+            throw "No valid request transport found.";
+            return null;
         }
 
         return XHR;
