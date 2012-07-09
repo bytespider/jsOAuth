fc63f3c89227b61a16da17afd8e39faf137d795e
Rob Griffiths <rob@bytespider.eu>
2012-07-09 14:42:07 +0200

    Decode query params before adding them to the query

--- src/OAuth/URI.js
+++ src/OAuth/URI.js
@@ -73,7 +73,7 @@
      */
     function QueryString(obj){
         var args = arguments, args_callee = args.callee, args_length = args.length,
-            i, querystring = this;
+            i, querystring = this, decode = OAuth.urlDecode;
 
         if (!(this instanceof args_callee)) {
             return new args_callee(obj);
@@ -82,7 +82,7 @@
         if (obj != undefined) {
             for (i in obj) {
                 if (obj.hasOwnProperty(i)) {
-                    querystring[i] = obj[i];
+                    querystring[i] = decode(obj[i]);
                 }
             }
         }
@@ -119,14 +119,14 @@
      */
     QueryString.prototype.setQueryParams = function (query) {
         var args = arguments, args_length = args.length, i, query_array,
-            query_array_length, querystring = this, key_value;
+            query_array_length, querystring = this, key_value, decode = OAuth.urlDecode;
 
         if (args_length == 1) {
             if (typeof query === 'object') {
                 // iterate
                 for (i in query) {
                     if (query.hasOwnProperty(i)) {
-                        querystring[i] = query[i];
+                        querystring[i] = decode(query[i]);
                     }
                 }
             } else if (typeof query === 'string') {
@@ -136,13 +136,13 @@
                 for (i = 0, query_array_length = query_array.length; i < query_array_length; i++) {
                     // split on '=' to get key, value
                     key_value = query_array[i].split('=');
-                    querystring[key_value[0]] = key_value[1];
+                    querystring[key_value[0]] = decode(key_value[1]);
                 }
             }
         } else {
             for (i = 0; i < args_length; i += 2) {
                 // treat each arg as key, then value
-                querystring[args[i]] = args[i+1];
+                querystring[args[i]] = decode(args[i+1]);
             }
         }
     };
