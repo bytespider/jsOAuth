66994c068e9127105424f3f0e9b2e2666d2fe95b
Michael Gauthier <mike@silverorange.com>
2013-10-28 15:41:03 -0300

    Fix variable names for Param constructor.

--- src/OAuth/Param.js
+++ src/OAuth/Param.js
@@ -1,28 +1,28 @@
     function Param(name, value) {
         var args = arguments, args_callee = args.callee, args_length = args.length,
-            i, queryparam = this;
+            i, param = this;
 
         if (!(this instanceof args_callee)) {
             return new args_callee(name, value);
         }
 
         if (name instanceof Array && name.length === 2) {
-            queryparam.name  = name[0] + '';
-            queryparam.value = name[1] + '';
+            param.name  = name[0] + '';
+            param.value = name[1] + '';
         } else if (name !== undefined) {
-            queryparam.name = name;
+            param.name = name;
             if (value === undefined) {
-                queryparam.value = '';
+                param.value = '';
             } else {
-                queryparam.value = value;
+                param.value = value;
             }
         }
 
-        return queryparam;
+        return param;
     }
 
     Param.prototype.copy = function() {
-        return new QueryParam(this.name, this.value);
+        return new Param(this.name, this.value);
     };
 
     Param.prototype.toString = function() {
